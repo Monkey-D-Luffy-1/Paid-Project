fce3aee35da6762eda9a47da7cdb0a05e0e3bae1
Dev Gajjar <145287513+DevGajjar28@users.noreply.github.com>
2024-12-20 22:43:35 +0530

    Create Password.js

---
+++ Password.js
@@ -0,0 +1,16 @@
+// Function to generate a random password
+function generatePassword(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+// Example usage
+const passwordLength = 12; // Set the desired length of your password
+const randomPassword = generatePassword(passwordLength);
+console.log('Generated Password:', randomPassword);
